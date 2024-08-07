cfedae96cb7175582373994327aaaf48ac3f0344
Naveen <pothurajunaveen963@gmail.com>
2024-08-07 08:39:43 +0000

    s

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -1,14 +1,37 @@
 // Write your code here
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import PlanetItem from '../PlanetItem'
 
 import './index.css'
 
 const PlanetSlider = props => {
   const {planetsList} = props
+  const settings = {
+    dots: false,
+  }
 
   return (
-    <div className="bg-con">
-      <PlanetItem planetsList={planetsList} />
+    <div data-testid="planets" className="bg-con">
+      <PlanetItem>
+        <Slider {...settings}>
+          {planetsList.map(planet => (
+            <div key={planet.id} className="planet-item">
+              <div className="img-con">
+                <img
+                  src={planet.imageUrl}
+                  alt={planet.name}
+                  className="planet-image"
+                />
+              </div>
+              <h1 className="name">{planet.name}</h1>
+              <p className="description">{planet.description}</p>
+            </div>
+          ))}
+        </Slider>
+      </PlanetItem>
     </div>
   )
 }
